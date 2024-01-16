95a693a3bb3715ec00be2d0fe62e9a74dd2573c2
Will <willrkramer@gmail.com>
2024-01-16 15:41:46 -0600

    Use local_ prefix for local ADC settings

--- src/Pico/main.py
+++ src/Pico/main.py
@@ -3,7 +3,7 @@ from array import array
 
 
 class main:
-    def __init__(self, adc_read_samples, adc_read_frequency):
+    def __init__(self, local_adc_read_samples, local_adc_read_frequency):
         # Pin definitions
         self.pin_sense_24V = Pin(27)
         self.adc_sense_24V = ADC(self.pin_sense_24V)
@@ -11,14 +11,14 @@ class main:
         self.adc_sense_5V = ADC(self.pin_sense_5V)
         
         # ADC read information
-        self.adc_read_samples = adc_read_samples
-        self.adc_read_frequency = adc_read_frequency
+        self.local_adc_read_samples = local_adc_read_samples
+        self.local_adc_read_frequency = local_adc_read_frequency
 
 
     def _read_ADC(self, adc):
         # Take self.adc_read_samples samples from the specified adc port and return the scaled mean (0-1 float)
         adc_counts = 0
-        samples_remaining = self.adc_read_samples
+        samples_remaining = self.local_adc_read_samples
         
         # Timer callback to take individual ADC samples. Once all samples have been taken, the timer object will be
         # deinitialized
@@ -36,14 +36,14 @@ class main:
                 timer.deinit()
                 
         # Create the timer object and begin acquiring ADC readings
-        Timer(-1).init(mode=Timer.PERIODIC, freq=self.adc_read_frequency, callback=timer_callback)
+        Timer(-1).init(mode=Timer.PERIODIC, freq=self.local_adc_read_frequency, callback=timer_callback)
         
         # Wait until all samples have been taken
         while samples_remaining > 0:
             pass
         
         # Once all samples have been taken successfully, return their scaled mean
-        return adc_counts / (self.adc_read_samples * 65535)
+        return adc_counts / (self.local_adc_read_samples * 65535)
 
 
     def read_ADC_5V(self):
